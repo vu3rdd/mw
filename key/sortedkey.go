a7c34609c2f1b17a3f78035dd5b5b5bf5ff399c9
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2022-11-29 20:26:28 +0530

    key fsm: add partial implementation of compute_key()

--- key/sortedkey.go
+++ key/sortedkey.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"log"
 	"reflect"
 
 	"github.com/qmuntal/stateless"
@@ -27,8 +28,7 @@ const (
 )
 
 type SortedKey struct {
-	appid string
-	sp    gospake2.SPAKE2
+	sp    *gospake2.SPAKE2
 }
 
 type pakeMsg struct {
@@ -38,6 +38,7 @@ type pakeMsg struct {
 // XXX we should also define GotPake() that gets called from Order FSM
 func InitMachine(initialState stateless.State) *stateless.StateMachine {
 	sortedKey := stateless.NewStateMachine(initialState)
+	sk := &SortedKey{}
 
 	sortedKey.Configure(StateS0KnowNothing).
 		Permit(TriggerGotCode, StateS1KnowCode)
@@ -47,20 +48,52 @@ func InitMachine(initialState stateless.State) *stateless.StateMachine {
 
 	// types for triggers
 	sortedKey.SetTriggerParameters(TriggerGotCode, reflect.TypeOf("code"), reflect.TypeOf("appid"))
+	sortedKey.SetTriggerParameters(TriggerGotPakeGood, reflect.TypeOf([]byte{}), reflect.TypeOf("side"))
 
 	// define actions on transitions
 	sortedKey.Configure(StateS1KnowCode).
 		OnEntryFrom(TriggerGotCode,  func(_ context.Context, args ...interface{}) error {
+			// build_pake
+			// M.add_message(pake)
 			code := args[0].(string)
 			appID := args[1].(string)
 
-			return buildAndSendPakeMsg(code, appID)
+			return sk.buildAndSendPakeMsg(code, appID)
 		})
 
-	return sortedKey
+	sortedKey.Configure(StateS2KnowKey).
+		OnEntryFrom(TriggerGotPakeGood, func(_ context.Context, args ...interface{}) error {
+			// computekey
+			// M.add_message(version)
+			// B.got_key
+			// R.got_key
+			msg2 := args[0].([]byte)
+			// side := args[1].(string)
+			key, err := sk.sp.Finish(msg2)
+			if err != nil {
+				log.Printf("spake2: %v\n", err)
+				return err
+			}
+
+			// XXX: call B.got_key(key)
+			fmt.Printf("Got key: %v\n", key)
+			phase := "version"
+			// dataKey := derivePhaseKey(key, side, phase)
+
+			// XXX use dataKey to encrypt the versions and
+			// send the encrypted message to the other
+			// side.
+			fmt.Printf("M.AddMessage(%s, \"encryptedData\")\n", phase)
+			fmt.Printf("R.GotKey(%s)\n", key)
+
+			return nil
+		})
+
+		return sortedKey
 }
 
-func buildAndSendPakeMsg(code string, appID string) error {
+
+func (sk *SortedKey) buildAndSendPakeMsg(code string, appID string) error {
 	// self._sp = SPAKE2_Symmetric(
         //         to_bytes(code), idSymmetric=to_bytes(self._appid))
 	// msg1 = self._sp.start()
@@ -68,6 +101,8 @@ func buildAndSendPakeMsg(code string, appID string) error {
         // self._M.add_message("pake", body)
 	pw := gospake2.NewPassword(code)
 	spake2 := gospake2.SPAKE2Symmetric(pw, gospake2.NewIdentityS(appID))
+
+	sk.sp = &spake2
 
 	body := spake2.Start()
 	pakeMsg := pakeMsg{ Body: hex.EncodeToString(body) }
@@ -78,7 +113,7 @@ func buildAndSendPakeMsg(code string, appID string) error {
 
 	// XXX call mailbox.AddMessage()
 	fmt.Printf("jsonMsg: %s\n", jsonMsg)
-	fmt.Printf("mailbox.AddMessage(\"pake\", %s)", hex.EncodeToString(jsonMsg))
+	fmt.Printf("mailbox.AddMessage(\"pake\", %s)\n", hex.EncodeToString(jsonMsg))
 
 	return nil
 }
